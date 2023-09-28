f9577c5919308a30d3e722e21c49f34419604cfc
LabyKreative <labykreative@yahoo.com>
2023-09-27 20:02:05 -0700

    Throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+// A function named guardrail that accepts one argument mathFunction (Function)
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
