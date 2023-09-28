0d105dfc3cc2e866610504465c7591d11a575273
LabyKreative <labykreative@yahoo.com>
2023-09-27 19:49:52 -0700

    Simple promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,4 @@
+// Using function signUpUser(firstName, lastName) {}; return fN & lN
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
+}
