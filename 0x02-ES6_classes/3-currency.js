be46e07688f718cddc17d31dbf41c747bf277d04
LabyKreative <labykreative@yahoo.com>
2023-09-28 14:28:45 +0000

    Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+/*
+ * Currency class
+ */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  /**
+   * Creates the full string representation of the Currency.
+   */
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
