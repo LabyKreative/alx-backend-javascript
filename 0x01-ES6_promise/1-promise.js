d4ed5f27e88f62ca294d7b08ee1dfafa0f79a64a
LabyKreative <labykreative@yahoo.com>
2023-09-27 19:41:10 -0700

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+// Return a promise. The parameter is a boolean. getFullResponseFromAPI(success)
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
