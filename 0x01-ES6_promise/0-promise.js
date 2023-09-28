5ef0f5e29a3fd92b6fd64ebf0ebbd0fb6f726e55
LabyKreative <labykreative@yahoo.com>
2023-09-27 19:00:11 -0700

    Keep every promise you make

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,22 +1,4 @@
-/* global XMLHttpRequest */
-// getResponseFromAPI() promise function
-async function getResponseFromAPI(url) {
-  const response = await new Promise((resolve, reject) => {
-    // Make the API request
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function onResponse() {
-      if (xhr.status === 200) {
-        // Success!
-        resolve(xhr.responseText);
-      } else {
-        // Failure!
-        reject(new Error(`API request failed with status code ${xhr.status}`));
-      }
-    };
-    xhr.send();
-  });
-  return response;
-}
+// Return  a Promise using function getResponseFromAPI()
+const getResponseFromAPI = () => new Promise((resolve) => resolve(true));
 
 export default getResponseFromAPI;
