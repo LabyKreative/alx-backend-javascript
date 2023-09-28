90a01d04fc9cfe7fe2c9c2cb9d958b68a27701be
LabyKreative <labykreative@yahoo.com>
2023-09-27 19:59:47 -0700

    Throw error / try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+// A function named divideFunction that accepts two arguments:
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
