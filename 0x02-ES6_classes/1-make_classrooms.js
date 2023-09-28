aede37257417a6348ae738f7093b8672a0833755
LabyKreative <labykreative@yahoo.com>
2023-09-28 14:16:09 +0000

    Let's make some classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+/*
+ * Import from ClassRoom
+ */
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
