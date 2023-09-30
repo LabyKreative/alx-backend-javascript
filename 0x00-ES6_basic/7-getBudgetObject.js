ece43ce250e46f04964da0695f379f07ae6ae9b3
LabyKreative <labykreative@yahoo.com>
2023-09-30 19:38:45 +0000

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+// Object property value shorthand syntax
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
