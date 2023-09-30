bf061723b4a441a153b4bdf22848c8e121d59d06
LabyKreative <labykreative@yahoo.com>
2023-09-30 19:18:42 +0000

    Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+// Block scope
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
+  }
+
+  return [task, task2];
+}
