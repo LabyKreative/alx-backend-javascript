3e9f238a6afe6fa786a3734f193788600efd3b11
LabyKreative <labykreative@yahoo.com>
2023-10-02 18:01:25 +0000

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+// Iterating through report objects
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
+}
