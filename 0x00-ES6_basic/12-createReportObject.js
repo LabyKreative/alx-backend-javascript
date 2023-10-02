b4a2a3ebd8ca3734088376675bae8c57c8e88ddf
LabyKreative <labykreative@yahoo.com>
2023-10-02 17:58:24 +0000

    Let's create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+// Iterator: eturn value of the previous function createEmployeesObject
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
