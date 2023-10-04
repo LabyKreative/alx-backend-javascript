33de2d9b593a9a53cd467026d5ad82ea3872a869
LabyKreative <labykreative@yahoo.com>
2023-10-04 02:06:58 +0000

    Reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+// getStudentIdsSum that returns the sum of all the student ids.
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
