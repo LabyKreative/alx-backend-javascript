6d655406519b926a441333e28ba9ee47fef833b3
LabyKreative <labykreative@yahoo.com>
2023-10-04 10:24:09 +0100

    Creating an interface for a student_2

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -57,7 +57,7 @@ const styleSheet = `
 /**
  * Displays information about students in a table.
  * @param students The list of students to display.
- * @author LabyKreative <https://github.com/LabyKreative>
+ * @author Adeyemi Owolabi <https://github.com/LabyKreative>
  */
 export const displayStudents = (students: Array<Student>): void => {
   const table = document.createElement('table');
