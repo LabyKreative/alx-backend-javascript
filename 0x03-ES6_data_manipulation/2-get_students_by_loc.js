7c39df4368e5288c632353142f9a29cf1b6572e4
LabyKreative <labykreative@yahoo.com>
2023-10-04 02:03:52 +0000

    Filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+// getStudentsByLocation returns an array of objects located in a specific city
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
