4c55e98f5b979ce0137478df9d6c0fa78d6fcf95
LabyKreative <labykreative@yahoo.com>
2023-10-04 02:17:47 +0000

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+// Returns a map of groceries with the following items (name, quantity):
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
