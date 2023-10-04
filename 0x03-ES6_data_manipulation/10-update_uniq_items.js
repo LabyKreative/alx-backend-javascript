bba9cfb5feeaa4cd06e6309747e827d1f16f67f8
LabyKreative <labykreative@yahoo.com>
2023-10-04 02:19:01 +0000

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+// Returns an updated map for all items with initial quantity at 1.
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
