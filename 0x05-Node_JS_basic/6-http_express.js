1a6de03571b5f9417e8345f92f8490296d092351
LabyKreative <labykreative@yahoo.com>
2023-11-22 00:32:26 +0000

    Creates a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
+app.get('/', (require, response) => {
   response.send('Hello Holberton School!');
 });
 
