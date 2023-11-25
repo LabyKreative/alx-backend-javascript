c5adc213da797a644f67874a9f4f8bd810c6511b
LabyKreative <109483013+LabyKreative@users.noreply.github.com>
2023-11-25 22:33:20 +0000

    Update 0-calcul.js: Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,4 +1,4 @@
-// Function to calculate number
+// Function to calculate number: takes two args(a & b)
 const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
